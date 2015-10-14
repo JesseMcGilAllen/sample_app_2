b7a8409b79eb5cfce108311d8f8ef3aaa1204975
Jesse McGil Allen <jmallen7@gmail.com>
2015-10-14 18:20:40 -0500

    adding login upon signup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,10 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  include ApplicationHelper
   fixtures :all
   
-  # Add more helper methods to be used by all tests here...
-  include ApplicationHelper
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
