b9ef97f89e529dc2ccb20c4e39853bd0fa284896
Jesse McGil Allen <jmallen7@gmail.com>
2015-10-13 19:51:04 -0500

    adding flash test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -28,5 +28,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
