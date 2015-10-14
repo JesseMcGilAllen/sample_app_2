ec8e687f9a15c851c8a6a3d25d7357454fd2acad
Jesse McGil Allen <jmallen7@gmail.com>
2015-10-14 12:59:39 -0500

    adding flash to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message
-    render 'new'
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
