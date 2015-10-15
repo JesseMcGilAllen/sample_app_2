77a689198199690845480662455f7b82145fe086
Jesse McGil Allen <jmallen7@gmail.com>
2015-10-14 21:16:29 -0500

    affirming tests pass using 2 different way of defining class methods (exercise 8.1).

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,21 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  class << self # part 2 of exercise 8.1
   # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  # def User.digest(string)
+  # def self.digest(string) # part 1 of exercise 8.1
+    def digest(string) # part 2 of exercise 8.1
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  def User.new_token
-    SecureRandom.urlsafe_base64
+    # def User.new_token
+    # def self.new_token # part 1 of exercise 8.1
+    def new_token # part 2 of exercise 8.1
+      SecureRandom.urlsafe_base64
+    end
   end
 
   def remember
